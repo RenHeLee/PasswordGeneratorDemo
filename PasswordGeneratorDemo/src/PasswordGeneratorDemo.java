debf1a3726659119394f4af4c34474803fd912d3
renhe <Acer@DESKTOP-T9N1S07>
2019-08-16 15:55:31 +0800

    Version 2

--- PasswordGeneratorDemo/src/PasswordGeneratorDemo.java
+++ PasswordGeneratorDemo/src/PasswordGeneratorDemo.java
@@ -165,10 +165,10 @@ public class PasswordGeneratorDemo {
                     key = sha512(temp);
                     key = sha512(usernameShaValue + passwordShaValue + purposeShaValue + key);
                 }
-                String[] uppercase = { "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "Z", "X", "C", "V", "B", "N", "M", "A", "S", "D", "F", "G", "H", "J", "K", "L" };
-                String[] lowercase = { "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "z", "x", "c", "v", "b", "n", "m", "a", "s", "d", "f", "g", "h", "j", "k", "l" };
+                String[] uppercase = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+                String[] lowercase = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
                 String[] digit = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
-                String[] symbol = { "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "[", "]", "{", "}", ";", ":", "<", ">", "?", ",", ".", "\\", "/" };
+                String[] symbol = { "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "[", "]", "{", "}", ";", ":", "<", ">", "?", ",", "." };
                 ArrayList<String> charList = new ArrayList<String>();
                 if (uppercaseCheckBox.isSelected()) {
                     charList.addAll(Arrays.asList(uppercase));
